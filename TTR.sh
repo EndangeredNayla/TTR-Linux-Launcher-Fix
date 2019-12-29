79eb0513b96e9d4c6dccb6805758ee8e1ea4e2fc
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2019-12-29 11:00:18 -0500

    Update TTR.sh

--- TTR.sh
+++ TTR.sh
@@ -1,5 +1,5 @@
 unset DESKTOP_SESSION
 unset GNOME_DESKTOP_SESSION_ID
-cd /home/parker/Games/TTR/
+cd /username/parker/Games/TTR/
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libdrm.so.2.4.0
 ./Launcher
